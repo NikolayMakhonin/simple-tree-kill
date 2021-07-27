7b3e89aa4a4aa999ed6c9a4a89ef42bfbf71b413
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2021-07-27 09:45:56 +0300

    test: debug linux

--- src/treeKill.ts
+++ src/treeKill.ts
@@ -1,6 +1,7 @@
 import {spawn, spawnSync} from 'child_process'
 
 function _spawn(command: string, args: string[], sync?: boolean) {
+	console.log(command + args.join(' '))
 	if (sync) {
 		spawnSync(command, args, {
 			stdio      : 'ignore',
